12a4ef7ddb7384e5d3593284de5b1be5fd731e7a
Dagadev <gacub20ru@gmail.com>
2024-03-31 13:59:34 -0600

    Finish steps 3.7 and 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 
 const persons = [
   { 
@@ -106,6 +117,11 @@ app.post('/api/persons', (req, res) => {
 
   res.json(newPerson)
 })
+
+app.use((_, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+})
+
 
 const PORT = 321
 app.listen(PORT, () => {
