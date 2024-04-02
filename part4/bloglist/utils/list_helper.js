039e9c313ea3d7813490a3ccf125666da5a26494
Dagadev <gacub20ru@gmail.com>
2024-04-02 16:36:24 -0600

    Finish step 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,46 @@ function totalLikes (blogs) {
   return blogs.reduce((acc, v) => acc + v.likes, 0)
 }
 
+function favoriteBlog (blogs) {
+  return blogs.reduce((acc, v) => v.likes > acc.likes ? v : acc)
+}
+
+function mostBlogs (blogs) {
+  return blogs.reduce((ac, v) => {
+    const blogsCount = blogs.filter(b => b.author === v.author).length
+
+    return blogsCount > ac.blogs
+      ? {
+          author: v.author,
+          blogs: blogsCount
+        }
+      : ac
+  }, {
+    author: '',
+    blogs: 0
+  })
+}
+
+function mostLikes (blogs) {
+  return blogs.reduce((ac, v) => {
+    const likesCount = blogs.filter(b => b.author === v.author).reduce((ac, v) => ac + v.likes, 0)
+
+    return likesCount > ac.likes
+      ? {
+          author: v.author,
+          likes: likesCount
+        }
+      : ac
+  }, {
+    author: '',
+    likes: 0
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
