ec56c76347481dbc29c7fd1af5935f979650f541
Dagadev <gacub20ru@gmail.com>
2024-04-06 14:08:48 -0600

    Finish step 5.14

--- part5/bloglist/src/components/blog.test.jsx
+++ part5/bloglist/src/components/blog.test.jsx
@@ -1,28 +1,46 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('show title and author of blog and hide extra info', async () => {
-  /** @type {Blog} */
-  const mockBlog = {
-    id: '661051gd308a2544g77e1989',
-    title: 'Blog title',
-    url: 'https://blog.url',
-    likes: 10,
-    author: 'Miguel Angel',
-    user: {
-      id: '123913',
-      name: 'David Garcia',
-      username: 'dagamdev'
-    }
+/** @type {Blog} */
+const mockBlog = {
+  id: '661051gd308a2544g77e1989',
+  title: 'Blog title',
+  url: 'https://blog.url',
+  likes: 10,
+  author: 'Miguel Angel',
+  user: {
+    id: '123913',
+    name: 'David Garcia',
+    username: 'dagamdev'
   }
+}
 
-  const { container } = render(<Blog blog={mockBlog} username={'dagamdev'} setBlogs={() => {}} />)
-  screen.debug()
-  screen.getByText(mockBlog.title)
-  screen.getByText(mockBlog.author)
+describe('blog component tests', () => {
+  /** @type {HTMLElement} */
+  let container
 
-  const extraInfo = container.querySelector('.info')
+  beforeEach(() => {
+    container = render(<Blog blog={mockBlog} username={'dagamdev'} setBlogs={() => {}} />).container
+  })
 
-  expect(extraInfo).toBeNull()
+  test('show title and author of blog and hide extra info', async () => {
+    screen.debug()
+    screen.getByText(mockBlog.title)
+    screen.getByText(mockBlog.author)
+
+    const extraInfo = container.querySelector('.info')
+
+    expect(extraInfo).toBeNull()
+  })
+
+  test('blog url and likes visible when clicking', async () => {
+    const user = userEvent.setup()
+    const showButton = screen.getByText('Show')
+    await user.click(showButton)
+
+    const info = container.querySelector('.info')
+    expect(info).toBeDefined()
+  })
 })
