8fc976cbf36cef5badd6d0c52bbfa737f5faa7ce
Dagadev <gacub20ru@gmail.com>
2024-04-07 19:49:03 -0600

    Finish step 5.18

--- part5/bloglist/cypress/e2e/spec.cy.js
+++ part5/bloglist/cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
+  const mockUser = {
+    username: 'dagamdev',
+    password: 'testpass'
+  }
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:321/api/testing/reset')
+    cy.request('POST', 'http://localhost:321/api/users', mockUser)
     cy.visit('http://localhost:5173')
   })
 
@@ -9,6 +15,28 @@ describe('Blog app', function() {
       cy.contains('Login in to application')
       cy.contains('Username')
       cy.contains('Password')
+    })
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('Login').click()
+
+      cy.contains('Username').find('input').type(mockUser.username)
+      cy.contains('Password').find('input').type(mockUser.password)
+      cy.get('form').submit()
+      cy.contains('successfully registered')
+      cy.get('.notification').should('have.css', 'background-color', 'rgba(0, 128, 0, 0.3)')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('Login').click()
+
+      cy.contains('Username').find('input').type(mockUser.username)
+      cy.contains('Password').find('input').type('incorrect')
+      cy.get('form').submit()
+      cy.contains('invalid username or password')
+      cy.get('.notification').should('have.css', 'background-color', 'rgba(255, 0, 0, 0.3)')
     })
   })
 })
