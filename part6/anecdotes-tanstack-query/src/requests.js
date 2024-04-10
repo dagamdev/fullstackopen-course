5e61727c40eb091eb7a1d509378870349211dcae
Dagadev <gacub20ru@gmail.com>
2024-04-10 11:29:00 -0600

    Finish step 6.21

--- part6/anecdotes-tanstack-query/src/requests.js
+++ part6/anecdotes-tanstack-query/src/requests.js
@@ -5,3 +5,10 @@ const baseUrl = 'http://localhost:321/anecdotes'
 export async function getAll () {
   return axios.get(baseUrl).then(res => res.data)
 }
+
+export async function createAnecdote (content) {
+  return axios.post(baseUrl, {
+    content,
+    votes: 0
+  })
+}
