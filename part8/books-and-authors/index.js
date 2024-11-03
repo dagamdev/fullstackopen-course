a5106e2d253eb1b8b81859b36113766962a26df8
gacub20ru <gacub20ru@gmail.com>
2024-11-03 18:54:19 +0000

    Finish exercise 8.2

--- part8/books-and-authors/index.js
+++ part8/books-and-authors/index.js
@@ -94,18 +94,26 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
-
-let count = 0
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
