06b852236d6ced575eb6a317830e4251e738e470
Dagadev <gacub20ru@gmail.com>
2024-11-04 16:46:13 -0600

    Finish exercise 8.19

--- part8/react-graphql/src/components/books.jsx
+++ part8/react-graphql/src/components/books.jsx
@@ -5,13 +5,25 @@ import { useState, useEffect } from "react"
 const Books = () => {
   const result = useQuery(ALL_BOOKS)
   const [books, setBooks] = useState([])
+  const [allGenres, setAllGenres] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState()
   
   useEffect(() => {
     console.log(result)
     if (result.data) {
       setBooks(result.data.allBooks)
+      const genres = []
+
+      for (const b of result.data.allBooks) {
+        // console.log(b)
+        for (const g of b.genres) {
+          if (!genres.includes(g)) genres.push(g)
+        }
+      }
+
+      setAllGenres(genres)
     }
-  }, [result])
+  }, [result.data])
 
   return (
     <div>
@@ -24,7 +36,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.filter(b => selectedGenre ? b.genres.includes(selectedGenre) : true).map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -33,6 +45,12 @@ const Books = () => {
           ))}
         </tbody>
       </table>
+      <ol style={{display: 'flex', gap: '12px', flexWrap: 'wrap', padding: '0'}}>
+        {allGenres.map(g => <button
+          key={g}
+          onClick={() => { setSelectedGenre(sg => sg === g ? undefined : g) }}
+        >{g}</button>)}
+      </ol>
     </div>
   )
 }
--- part8/react-graphql/src/queries.js
+++ part8/react-graphql/src/queries.js
@@ -21,6 +21,7 @@ export const ALL_BOOKS = gql`
         name
         born
       }
+      genres
       published
     }
   }
