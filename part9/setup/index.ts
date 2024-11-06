9a460f5ae80350a0036bed147e9217ddf3379484
Dagadev <gacub20ru@gmail.com>
2024-11-05 19:50:53 -0600

    Finish exercise 9.7

--- part9/setup/index.ts
+++ part9/setup/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { calculateBmi } from './bmiCalculator'
+import { calculateExercises } from './exerciseCalculator'
 
 const app = express()
-// app.use(express.json())
+app.use(express.json())
 
 app.get('/hello', (_req, res) => {
   res.send('Hello world')
@@ -11,7 +12,6 @@ app.get('/hello', (_req, res) => {
 app.get('/bmi', (req, res) => {
   const height = Number(req.query.height)
   const weight = Number(req.query.weight)
-  console.log(height, weight)
 
   if (isNaN(height)) {
     res.status(404).json({ error: 'The height query parama is not number' })
@@ -28,6 +28,29 @@ app.get('/bmi', (req, res) => {
     weight,
     bmi
   })
+})
+
+app.post('/exercises', (req, res) => {
+  const dailyExercises = req.body?.daily_exercises
+  const target = req.body?.target
+  const days: number[] = []
+
+  try {
+    if (typeof target !== 'number') throw new Error('The target field is not a number')
+    if (!(dailyExercises instanceof Array)) throw new Error('The daily_exercises field is not a number array')
+
+    for (const exDay of dailyExercises) {
+      const day = Number(exDay)
+      if (isNaN(day)) throw new Error(`The hour ${exDay} is not a number`)
+      days.push(day)
+    }
+
+    res.json(calculateExercises(days, target))
+  } catch (error) {
+    if (error instanceof Error) {
+      res.json({ error: error.message })
+    } else res.json({ error: 'An error has occurred' })
+  }
 })
 
 const PORT = 400
